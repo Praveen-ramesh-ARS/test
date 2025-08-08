8f056b7534ebe99053d6314cb9b0fea875cb65b5
praveen-ramesh-ARS <visit.praveen47@gmail.com>
2025-08-08 19:41:34 +0530

    add_build_files

--- Weather_app/wheather_project/settings.py
+++ Weather_app/wheather_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$!5axp0k7@wude&k0pni-x@zb#elhyuoq^pxa#1(uqy-g!m=q_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['weather-app1-z2v4.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
